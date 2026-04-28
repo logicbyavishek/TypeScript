b61c1aea57ea0f6a8f0941d1e6adeea42c5f604b
AVISHEK DUTTA <109311355+logicbyavishek@users.noreply.github.com>
2026-04-28 18:50:05 +0530

    Interface with extends

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 "use strict";
-const transactions1 = {
-    payerAccountNumber: 123,
-    payeeAccountNumber: 455,
+const audioBook = {
+    name: "Thank you for leaving",
+    price: 400,
+    filesize: 45,
+    format: "MP4",
+    duration: "3 Hour"
 };
-const transactions2 = {
-    payerAccountNumber: 654,
-    payeeAccountNumber: 455,
-};
-const bankAccount = {
-    accountNumber: 455,
-    accountHolder: "Avi",
-    balance: 2000,
-    isActive: true,
-    transactions: [transactions1, transactions2]
-};
-console.log(bankAccount);
+console.log(audioBook);
--- app.ts
+++ app.ts
@@ -1,45 +1,23 @@
-interface Transaction{
-    payerAccountNumber:number;
-    payeeAccountNumber:number;
+interface Book{
+    name:string;
+    price:number;
 }
 
-interface BankAccount{
-    accountNumber:number;
-    accountHolder:string;
-    balance:number;
-    isActive:boolean;
-    transactions:Transaction[]
+interface EBook extends Book{
+    filesize:number;
+    format:string;
 }
 
-const transactions1:Transaction = {
-    payerAccountNumber:123,
-    payeeAccountNumber:455,
+interface AudioBook extends EBook{
+    duration:string
 }
 
-const transactions2:Transaction ={
-    payerAccountNumber:654,
-    payeeAccountNumber:455,
+const audioBook:AudioBook ={
+    name:"Thank you for leaving",
+    price:400,
+    filesize:45,
+    format:"MP4",
+    duration:"3 Hour"
 }
 
-const bankAccount:BankAccount={
-    accountNumber:455,
-    accountHolder:"Avi",
-    balance:2000,
-    isActive:true,
-    transactions:[transactions1,transactions2]
-}
-
-console.log(bankAccount)
-
-/**
- * {
-  accountNumber: 455,
-  accountHolder: 'Avi',
-  balance: 2000,
-  isActive: true,
-  transactions: [
-    { payerAccountNumber: 123, payeeAccountNumber: 455 },
-    { payerAccountNumber: 654, payeeAccountNumber: 455 }
-  ]
-}
- */
+console.log(audioBook)
